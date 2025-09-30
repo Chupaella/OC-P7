869e7f4e6e0b695464e4c017b383de5237ac232d
Chupaellla <109806467+Chupaellla@users.noreply.github.com>
2025-09-30 04:10:21 +0200

    git page prep

--- p7/src/router.jsx
+++ p7/src/router.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Header from "./components/Header.jsx"
-import Home from "./pages/Home.jsx"
-import Logement from "./pages/Logement.jsx"
-import About from "./pages/About.jsx"
-import NotFound from "./pages/NotFound.jsx"
-import Footer from "./components/Footer.jsx"
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Logement from "./pages/Logement.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 export default function AppRouter() {
     return (
-        <BrowserRouter>
+        <Router>
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/logement/:id" element={<Logement />} />
                 <Route path="/a-propos" element={<About />} />
+                <Route path="/logement/:id" element={<Logement />} />
                 <Route path="/404" element={<NotFound />} />
                 <Route path="*" element={<Navigate to="/404" replace />} />
             </Routes>
             <Footer />
-        </BrowserRouter>
-    )
+        </Router>
+    );
 }
